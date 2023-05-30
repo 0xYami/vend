23d40ab21314758262ad9863b2de8f4178a950dc
0xYami <0xYami@proton.me>
2023-05-30 19:13:09 +0200

    refactor: rename repository to `ichiba`

--- src/main.rs
+++ src/main.rs
@@ -25,7 +25,7 @@ async fn main() {
     tracing_subscriber::registry()
         .with(
             tracing_subscriber::EnvFilter::try_from_default_env()
-                .unwrap_or_else(|_| "vend=debug,axum::rejection=trace,tower_http=debug".into()),
+                .unwrap_or_else(|_| "ichiba=debug,axum::rejection=trace,tower_http=debug".into()),
         )
         .with(tracing_subscriber::fmt::layer())
         .init();
